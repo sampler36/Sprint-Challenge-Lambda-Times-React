360e48a3bda774b2422ad2844fd59572d9a92273
Talent Antonio <antonio.talenty@gmail.com>
2019-03-01 11:13:16 +0000

    First Arrow Tab

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import Tab from './Tab';
-const Tabs = props => {
+import PropTypes from "prop-types";
+
+const Tabs = ({ tabs, selectedTab, selectTabHandler }) => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {tabs.map((tab, index) => (
+          <Tab key={index}  tab={tab} 
+          selectTabHandler={selectTabHandler}
+           selectedTab={selectedTab}
+        />
+              ))}
       </div>
     </div>
   );
 };
 
 // Make sure to use PropTypes to validate your types!
+Tabs.propTypes = {
+  tabs: PropTypes.array,
+  selectedTab: PropTypes.string,
+  selectTabHandler: PropTypes.func
+  }
 export default Tabs;
