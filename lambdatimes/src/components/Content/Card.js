ae2ac7ea02511278b6b6b9a3c0fe0696098633ea
Talent Antonio <antonio.talenty@gmail.com>
2019-03-02 11:33:06 +0000

    Card Author and Icon correction

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -14,9 +14,9 @@ const Card = ({ card }) => {
       <div className="headline">{card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={Card.img} alt="" />
+          <img src={card.img} alt="" />
         </div>
-        <span>By {Card.author}</span>
+        <span>By {card.author}</span>
       </div>
     </div>
   );
--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Tab = ({ tab, selectTabHandler,selectedTab }) => {
+const Tab = ({ tab, selectTabHandler}) => {
   // Make sure you include PropTypes on your props.
   Tab.propTypes = {
     tab: PropTypes.string,
@@ -11,8 +11,12 @@ const Tab = ({ tab, selectTabHandler,selectedTab }) => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+
+  
   return (
     <div className="tab active-tab"
+     
+
       onClick={() => {
         /* Replace this dummy click handler function with your selectTabHandler function from props 
          you'll need to pass the `tab` in as an argument to this handler. */
